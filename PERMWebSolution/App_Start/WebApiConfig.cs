4d2737c6deb6a185e8d7e8bee54dbfe0eb6830b9
Povilas <p.treigys@eif.viko.lt>
2016-02-17 20:35:36 +0200

    added tracing  config

--- PERMWebSolution/App_Start/WebApiConfig.cs
+++ PERMWebSolution/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace PERMWebSolution
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
             //This package allows the ASP.NET Web API framework to trace to System.Diagnostics.Trace.
             //http://go.microsoft.com/fwlink/?LinkId=269874
             config.EnableSystemDiagnosticsTracing();
