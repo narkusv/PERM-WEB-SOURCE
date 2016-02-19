a540eb61b2d2ba6735d9518aa2b1974c8400d298
Povilas <p.treigys@eif.viko.lt>
2016-02-19 19:06:32 +0200

    Added erro handling script

---
+++ PERMWebSolution/scripts/errohandling.js
@@ -0,0 +1,10 @@
+﻿function handleException(request, message, error) {
+    var msg = "";
+    msg += "Code: " + request.status + "\n";
+    msg += "Text: " + request.statusText + "\n";
+    if (request.responseJSON != null) {
+        msg += "Message" +
+            request.responseJSON.Message + "\n";
+    }
+    alert(msg);
+}
