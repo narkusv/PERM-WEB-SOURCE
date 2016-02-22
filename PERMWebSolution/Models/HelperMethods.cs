942391477b5a8c59ee958e091114bce76e65cde3
Povilas <p.treigys@eif.viko.lt>
2016-02-22 12:38:12 +0200

    Added SHA string encryption

--- PERMWebSolution/Models/HelperMethods.cs
+++ PERMWebSolution/Models/HelperMethods.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace PERMWebSolution.Models
@@ -17,6 +19,26 @@ namespace PERMWebSolution.Models
             var random = new Random();
             return  new string(Enumerable.Repeat(chars, 24)
               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        /// <summary>
+        /// Encrypts string with SHA256 algorithm
+        /// </summary>
+        /// <param name="value">String to be encrypted</param>
+        /// <returns>SHA256 encrypted string</returns>
+        public static string sha256_hash(string value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
         }
     }
 }
