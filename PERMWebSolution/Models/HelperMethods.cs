c7f8dfc35a755210f5b6e4aeb4e5f28b44492b40
Povilas <p.treigys@eif.viko.lt>
2016-02-22 12:28:01 +0200

    Added helper method static class for common static methods

---
+++ PERMWebSolution/Models/HelperMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PERMWebSolution.Models
+{
+    public static class HelperMethods
+    {
+        /// <summary>
+        /// Method for generating new session id
+        /// </summary>
+        /// <returns>string sessionID</returns>
+        public static string generateSessionID()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return  new string(Enumerable.Repeat(chars, 24)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
