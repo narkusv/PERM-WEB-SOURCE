1545a4f08564705a5e31f4bbe5e8d900fce3664f
Povilas <p.treigys@eif.viko.lt>
2016-02-23 19:34:27 +0200

    Added comments why standard Routing was swiched off

--- PERMWebSolution/App_Start/WebApiConfig.cs
+++ PERMWebSolution/App_Start/WebApiConfig.cs
@@ -13,15 +13,19 @@ namespace PERMWebSolution
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+         
+            //Commented standard route because it does not siuts our needs. All Routes are resolved in each controller class
 
-            config.Routes.MapHttpRoute(
+          /*  config.Routes.MapHttpRoute(
                 name: "PERMApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+            ); //standard route*/
+
 
             //This package allows the ASP.NET Web API framework to trace to System.Diagnostics.Trace.
             // more at http://go.microsoft.com/fwlink/?LinkId=269874
+            //if needed we can easily configure request file logger
             config.EnableSystemDiagnosticsTracing();
         }
     }
