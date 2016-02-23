27811cf7d3fc5dd53f038108541028c1313396df
Povilas <p.treigys@eif.viko.lt>
2016-02-23 18:03:05 +0200

    DBContext added with comments

--- PERMWebSolution/Models/DBContext.cs
+++ PERMWebSolution/Models/DBContext.cs
@@ -2,29 +2,36 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.Configuration;
 using System.Configuration;
 
 namespace PERMWebSolution.Models
 {
     /// <summary>
-    /// Class for DB connection
+    /// Class for DB connection management and data querying
     /// </summary>
     public static class DBContext
     {
         /// <summary>
-        /// static variable holds connection string
+        /// Static variable holds connection string 
         /// </summary>
-        private static string conString = ConfigurationManager.ConnectionStrings["PERMLOCALServer"].ConnectionString;
+        private static string conString = ConfigurationManager.ConnectionStrings["PERMLOCALServer"].ConnectionString; 
+        //Connection string name should be configured to everyone local account configuration must be done in <--Web.config--> file
 
-
+        /// <summary>
+        /// Method executes query on database that does not have any parameters
+        /// </summary>
+        /// <param name="query">Query to execute</param>
+        /// <returns>DataTable object</returns>
         public static DataTable ExecuteQueryTable(string query)
         {
             return ExecuteQueryTable(query, null);
         }
-
+        /// <summary>
+        /// Method executes query on database that has parameters
+        /// </summary>
+        /// <param name="query">Query to execute</param>
+        /// <param name="parameters">Parameters to substitute into query</param>
+        /// <returns>DataTable object</returns>
         public static DataTable ExecuteQueryTable(string query, Dictionary<string, object> parameters)
         {
             DataTable tbl = new DataTable();
@@ -36,7 +43,7 @@ namespace PERMWebSolution.Models
 
                     using (SqlCommand cmd = conn.CreateCommand())
                     {
-                        cmd.CommandText = query;
+                        cmd.CommandText = query; //if procedure will be called this must be changed to procedure
 
                         if (parameters != null)
                         {
@@ -47,14 +54,14 @@ namespace PERMWebSolution.Models
                         }
                         using (SqlDataAdapter da = new SqlDataAdapter(cmd))
                         {
-                            da.Fill(tbl);
+                            da.Fill(tbl); //actually executes query
                         }
                     }
                 }
             }
             catch (Exception ex)
             {
-                return null; // Console.WriteLine(ex.Message);
+                return null; // If null is returned that mean query failed!
             }
             return tbl;
         }
