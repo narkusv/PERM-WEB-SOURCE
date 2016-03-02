f03238c68286009c92ace493b0705e8e8d4bd1ff
narkusv <narkus.v@gmail.com>
2016-03-02 15:37:38 +0200

    PhaseActivity view added

---
+++ PERMWebSolution/Controllers/PhaseActivityController.cs
@@ -0,0 +1,32 @@
+﻿using PERMWebSolution.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PERMWebSolution.Controllers
+{
+    public class PhaseActivityController : ApiController
+    {
+        [HttpGet]
+        [Route("api/PhaseActivity/GetPhaseActivity/")] //if you pass string by GET method from Ajax, MAKE SURE, that Ajax URI ends with leading "/" symbol ->>> it is important, otherwise this route method will not be resolved!
+        public IHttpActionResult GetPhaseActivity()
+        {
+            //var a = table;
+            IHttpActionResult ret = null;
+
+            List<PhaseActivity> phaseActivityList = PhaseActivity.getPhaseActivity();
+            if (phaseActivityList.Any())
+            {
+                ret = Ok(phaseActivityList);
+            }
+            
+
+      
+            return ret;
+        }
+
+    }
+}
---
+++ PERMWebSolution/Models/PhaseActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PERMWebSolution.Models
+{
+    public class PhaseActivity
+    {
+        public int activityId { get; set; }
+        public string Name { get; set; }
+        public  string isActivated { get; set; }
+
+
+
+
+        public static List<PhaseActivity> getPhaseActivity()
+        {
+            List<PhaseActivity> activities = new List<PhaseActivity>();
+            Random rand = new Random();
+            for(int i = 0; i <=6; i++)
+            {
+                activities.Add(new PhaseActivity { activityId = i, Name = (rand.Next(1, 10) > 5) ? "Cheeseburgers" : "", isActivated = (rand.Next(0, 10)>5 ? "checked" : "") });
+
+            }
+
+
+            return activities;
+
+        }
+    }
+
+   
+}
